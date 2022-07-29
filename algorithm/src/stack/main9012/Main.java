da64f2218e6846ca6ba739b3f013867cb777ff97
cohouseol <cohouseol@gmail.com>
2022-07-29 10:35:52 +0900

    stack 9012

---
+++ algorithm/src/stack/main9012/Main.java
@@ -0,0 +1,56 @@
+package stack.main9012;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    static int N,M;
+
+    static StringBuilder sb = new StringBuilder();
+    static Scanner scan = new Scanner(System.in);
+
+
+    public static void input(String input) {
+        Scanner scan = new Scanner(System.in);
+        N = scan.nextInt();
+        scan.nextLine();
+        for(int i = 0 ; i < N ; i++){
+            chk(scan.nextLine());
+        }
+
+    }
+    static void chk(String s){
+        int i = 0;
+        Stack<Character> stack = new Stack<>();
+        while(i < s.length()){
+            char c = s.charAt(i);
+            if(c=='('){
+                stack.push(c);
+            } else {
+                if(stack.size() <1){
+                    System.out.println("NO");
+                    return ;
+                }
+                stack.pop();
+            }
+            i++;
+        }
+        if(stack.size() >0){
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
+        }
+    }
+
+    public static void main(String[] args) {
+        N = scan.nextInt();
+        int i = 0;
+        while( i <N) {
+            i++;
+            input(scan.nextLine());
+
+        }
+    }
+
+}
